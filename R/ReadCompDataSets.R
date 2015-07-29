60f34b0129b687feb2bb5dd4ae425e016f36c155
HajkD <hajkdrost@googlemail.com>
2015-07-29 13:32:29 +0100

    adding first function skeleton

---
+++ R/ReadCompDataSets.R
@@ -0,0 +1,8 @@
+#' @title Read multiple expression data Sets that shall be compared
+#' @description This function 
+#' @author Hajk-Georg Drost
+#' @export
+ReadCompDataSets <- function(){
+  
+  
+}
